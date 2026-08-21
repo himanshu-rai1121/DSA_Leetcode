05c6fbdacbb27658364eb922a420db81e48195fb
Himanshu <95352548+himanshu-rai1121@users.noreply.github.com>
2026-08-21 20:13:50 +0530

    Time: 10 ms (67.62%), Space: 57.4 MB (11.39%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -15,11 +15,37 @@ class Solution {
         int notTake = solve(amount, coins, index-1, dp);
         return dp[index][amount] = take + notTake;
     }
+
+    private int solveTab(int target, int coins[], int[][] dp) {
+
+        for(int i=0;i<coins.length;i++)
+            dp[i][0] = 1;
+        // Base case for first coin
+        for (int amount = 0; amount <= target; amount++) {
+            if (amount % coins[0] == 0) {
+                dp[0][amount] = 1;
+            }
+        }
+
+        for(int index = 1;index<coins.length;index++) {
+            for(int amount = 0;amount<=target;amount++) {
+                int take = 0;
+                if(amount>=coins[index])
+                    take = dp[index][amount-coins[index]];
+
+                int notTake = dp[index-1][amount];
+                dp[index][amount] = take + notTake;
+            }
+        }        
+        return dp[coins.length-1][target];
+    }
     public int change(int amount, int[] coins) {
         int[][] dp = new int[coins.length][amount+1];
-        for(int[] arr: dp)
-            Arrays.fill(arr, -1);
 
-        return solve(amount, coins, coins.length-1, dp);
+        return solveTab(amount, coins, dp);
+
+        // for(int[] arr: dp)
+        //     Arrays.fill(arr, 0);
+        // return solve(amount, coins, coins.length-1, dp);
     }
 }

1ca636519cea6090220e57f9b616c134e0a084a6
Himanshu <95352548+himanshu-rai1121@users.noreply.github.com>
2026-08-21 18:44:08 +0530

    Time: 18 ms (43.19%), Space: 46.8 MB (38.84%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -15,14 +15,40 @@ class Solution {
 
         return dp[index][remainingTarget] = Math.min(take, notTake);
     }
+
+    private int solveTab(int[] coins, int amount, int[][]dp) {
+
+        for(int target=0;target<=amount;target++){
+            if(target%coins[0] == 0) dp[0][target] = target/coins[0];
+            else dp[0][target] = 10000008;
+        }
+
+
+        for(int index = 1; index<coins.length; index++) {
+            for(int target=0; target<=amount;target++) {
+
+                int take = 10000008;
+                if(coins[index]<=target) 
+                take = 1 + dp[index][target-coins[index]];
+
+                int notTake = dp[index-1][target];
+
+                dp[index][target] = Math.min(take, notTake);
+            }
+        }
+        return dp[coins.length-1][amount];
+    }
+
     public int coinChange(int[] coins, int amount) {
 
         int[][] dp = new int[coins.length][amount+1];
         for(int[] arr: dp)
             Arrays.fill(arr, -1);
-            
-        int ans =  solve(coins, amount, coins.length-1, dp);
-        if(ans==100000008)
+
+        int ans = solveTab(coins, amount, dp);
+
+        // int ans =  solve(coins, amount, coins.length-1, dp);
+        if(ans>=10000008)
             return -1;
         return ans;
     }
